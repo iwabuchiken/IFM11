46f83699f35bd35b84e8fed9b3dfa6808dc2b258
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-08 08:04:12 +0900

    D-1(m)
    v-1.0
    ---
    1. Initial
    2. Installed

---
+++ src/ifm11/main/MainActv.java
@@ -0,0 +1,197 @@
+package ifm11.main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Vibrator;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+
+public class MainActv extends ListActivity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	/*----------------------------
+		 * 1. super
+		 * 2. Set content
+		 * 2-2. Set title
+		 * 3. Initialize => vib
+		 * 
+		 *  4. Set list
+		 *  5. Set listener => Image buttons
+		 *  6. Set path label
+		 *  
+		 *  7. Initialize preferences
+		 *  
+		 *  8. Refresh DB
+			----------------------------*/
+		
+        super.onCreate(savedInstanceState);
+        
+        this.setTitle(this.getClass().getName());
+        
+        setContentView(R.layout.actv_main);
+        
+    }//public void onCreate(Bundle savedInstanceState)
+
+    private void do_debug() {
+		
+		/***************************************
+		 * Processing
+		 ***************************************/
+		File dname = new File("/mnt/sdcard-ext/processing");
+		
+		if (dname.exists()) {
+			
+			for (String fname : dname.list()) {
+				
+				// Log
+				Log.d("MainActv.java"
+						+ "["
+						+ Thread.currentThread().getStackTrace()[2]
+								.getLineNumber()
+						+ ":"
+						+ Thread.currentThread().getStackTrace()[2]
+								.getMethodName() + "]",
+					"fname=" + fname);
+				
+			}
+			
+		} else {//if (dname.exists())
+			
+			// Log
+			Log.d("MainActv.java" + "["
+					+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+					+ ":"
+					+ Thread.currentThread().getStackTrace()[2].getMethodName()
+					+ "]", dname.getName() + " => Doesn't exist");
+			
+		}//if (dname.exists())
+    	
+	}//private void do_debug()
+
+	@Override
+	protected void onListItemClick(ListView lv, View v, int position, long id) {
+		/*----------------------------
+		 * Steps
+		 * 0. Vibrate
+		 * 
+		 * 1. Get item name
+		 * 2. Get file object
+		 * 3. Is a directory?
+		 * 		=> If yes, update the current path
+			----------------------------*/
+		// Log
+		Log.d("MainActv.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "onListItemClick()");
+//		//
+//		vib.vibrate(CONS.vibLength_click);
+//		
+//		String itemName = (String) lv.getItemAtPosition(position);
+		
+		
+		super.onListItemClick(lv, v, position, id);
+	}//protected void onListItemClick(ListView l, View v, int position, long id)
+
+	@Override
+	protected void onDestroy() {
+		/*----------------------------
+		 * 1. Reconfirm if the user means to quit
+		 * 2. super
+		 * 3. Clear => prefs
+		 * 4. Clear => file_names
+			----------------------------*/
+		
+		super.onDestroy();
+		
+	}//protected void onDestroy()
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		
+//		Methods.confirm_quit(this, keyCode);
+		
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// 
+//		MenuInflater mi = getMenuInflater();
+//		mi.inflate(R.menu.main_menu, menu);
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		
+
+		}//switch (item.getItemId())
+		
+		return super.onOptionsItemSelected(item);
+		
+	}//public boolean onOptionsItemSelected(MenuItem item)
+
+	@Override
+	protected void onPause() {
+		
+		super.onPause();
+
+		Log.d("MainActv.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "onPause()");
+		
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO ?¿½?¿½?¿½?¿½?¿½?¿½?¿½?¿½?¿½?¿½?¿½ê‚½?¿½?¿½?¿½\?¿½b?¿½h?¿½E?¿½X?¿½^?¿½u
+		super.onResume();
+
+	}//protected void onResume()
+
+	@Override
+	protected void onStart() {
+		
+		super.onStart();
+		
+		// Log
+		Log.d("[" + "MainActv.java : "
+				+ +Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ "]", "onStart!");
+		
+	}//protected void onStart()
+
+}//public class MainActv extends Activity
