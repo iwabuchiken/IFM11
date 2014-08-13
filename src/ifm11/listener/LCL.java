c169d264435645fa615e23097d1ba49e7f962fd5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-08-13 10:01:31 +0900

    D-10
    v-1.3
    ---
    1. move mode => long click
    	==> all select/all de-select
    
    	==> done

--- src/ifm11/listener/LCL.java
+++ src/ifm11/listener/LCL.java
@@ -1,6 +1,7 @@
 package ifm11.listener;
 
 import ifm11.utils.CONS;
+import ifm11.utils.Tags;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Vibrator;
@@ -27,7 +28,53 @@ public class LCL implements OnLongClickListener {
 		
 		CONS.Admin.vib.vibrate(CONS.Admin.vibLength_click);
 		
+		Tags.ButtonTags tag = (Tags.ButtonTags) v.getTag();
+		
+		switch (tag) {
+		
+		case TILIST_CB:
+			
+			case_TILIST_CB();	
+			
+			break;
+			
+		default:
+			break;
+			
+		}
+//		TILIST_CB
+		
 		return false;
+	}
+
+	private void 
+	case_TILIST_CB() {
+		// TODO Auto-generated method stub
+		
+		if (CONS.TNActv.checkedPositions.contains((Integer) position)) {
+
+			CONS.TNActv.checkedPositions.clear();
+			
+//			TNActv.aAdapter.notifyDataSetChanged();
+			CONS.TNActv.adp_TNActv_Main_Move.notifyDataSetChanged();
+			
+		/*----------------------------
+		 * 2. If not
+			----------------------------*/
+		} else {//if (CONS.TNActv.checkedPositions.contains((Integer) position))
+			
+			CONS.TNActv.checkedPositions.clear();
+			
+			for (int i = 0; i < CONS.TNActv.list_TNActv_Main.size(); i++) {
+				
+				CONS.TNActv.checkedPositions.add((Integer) i);
+				
+			}//for (int i = 0; i < TNActv.tiList.size(); i++)
+			
+//			TNActv.aAdapter.notifyDataSetChanged();
+			CONS.TNActv.adp_TNActv_Main_Move.notifyDataSetChanged();
+			
+		}//if (CONS.TNActv.checkedPositions.contains((Integer) position))	
 	}
 
 }
